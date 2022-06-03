50d3494f6a439e37a1bb28605e9ee5e41776f00f
kd0427 <rlaemek@naver.com>
2022-06-03 16:20:02 +0900

    백준 조건문

--- src/day1/Main3.java
+++ src/day1/Main3.java
@@ -1,10 +1,35 @@
 package day1;
 
+import javax.swing.JOptionPane;
+
 public class Main3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int score;
+		
+		while(true) {
+			score = Integer.parseInt(JOptionPane.showInputDialog("점수를 입력하세요."));
+			
+			if(score >=0 && score <=100) {
+				break;
+			}else {
+				System.out.println("0~100사이의 수를 입력하세요.");
+			}
+		}
+		
+		if(score >=90 && score <=100) {
+			System.out.println("A");
+		}else if(score >=80 && score <=89) {
+			System.out.println("B");
+		}else if(score >=70 && score <=79) {
+			System.out.println("C");
+		}else if(score >=60 && score <=69) {
+			System.out.println("D");
+		}else {
+			System.out.println("F");
+		}
+		
+		
+	
 	}
-
 }
---
+++ src/day1/Main5.java
@@ -0,0 +1,30 @@
+package day1;
+
+public class Main5 {
+
+	public static void main(String[] args) {
+
+		int num = (int) (Math.random() * 6) + 1;
+
+		switch (num) {
+			case 1:
+				System.out.println("1입니다.");
+				break;
+			case 2:
+				System.out.println("2입니다.");
+				break;
+			case 3:
+				System.out.println("3입니다.");
+				break;
+			case 4:
+				System.out.println("4입니다.");
+				break;
+			case 5:
+				System.out.println("5입니다.");
+				break;
+			case 6:
+				System.out.println("6입니다.");
+				break;
+		}
+	}
+}
